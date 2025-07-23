68ceb757e7de77a907b56c7e168f3ff27c72f79a
Josh Gold <jogold@jogold-mn2.linkedin.biz>
2025-07-22 21:12:27 -0700

    Force deployment - meal costs should be fixed for line graph

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { WelcomeProvider } from './features/welcome/context/WelcomeContext'
 import AuthenticationGuard from './components/AuthenticationGuard'
 import AuthenticatedApp from './components/AuthenticatedApp'
 
-// Force deployment to clear caching issues - API now working with ingredients - Final fix
+// Force deployment to clear caching issues - API now working with ingredients - Meal costs fixed - Line graph should show consumed values
 
 // Import pages
 import Login from './pages/Login'
